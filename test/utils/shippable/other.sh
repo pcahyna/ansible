ea09661552ce77ad64111edc8c6b86bfbcc03605
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-01-24 09:33:22 +0800

    additional CI dockerfile/script updates

--- test/utils/shippable/other.sh
+++ test/utils/shippable/other.sh
@@ -15,8 +15,6 @@ retry.py apt-get install -qq \
     libssl-dev \
     libffi-dev \
 
-pip install cryptography
-
 ln -sf x86_64-linux-gnu-gcc-4.9 /usr/bin/x86_64-linux-gnu-gcc
 
 retry.py pip install tox --disable-pip-version-check
